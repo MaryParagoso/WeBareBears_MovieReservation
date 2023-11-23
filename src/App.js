ab8117d0ef0708377830db726b832193ca0a0a0b
Seth Angelo Llenos <seth.llenos@awsys-i.com>
2023-11-23 16:28:47 +0800

    Menu Page and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing';
+import MenuPage from './pages/MenuPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing/>} ></Route>
+        <Route path="/menu" element={<MenuPage/>} ></Route>
       </Routes>
     </Router>
   );
---
+++ src/pages/MenuPage.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import MenuScreen from '../component/MenuScreen';
+import ReservationPath from '../component/ReservationPath';
+import CancellationPath from '../component/CancellationPath';
+
+class MenuPage extends Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize state using the constructor
+    this.state = {
+      selectedOption: null,
+      selectedDate: null,
+      selectedReservation: null,
+    };
+
+    // Bind the method to the current instance
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  // Method to handle button click
+  handleButtonClick(choice) {
+    this.setState({
+      selectedOption: choice,
+    });
+  }
+
+  render() {
+    const { selectedOption, selectedDate, selectedReservation } = this.state;
+
+    return (
+      <div>
+        {selectedOption === null ? (
+          <MenuScreen onButtonClick={this.handleButtonClick} />
+        ) : (
+          <div>
+            {selectedOption === 'Reserve' ? (
+              <ReservationPath
+                selectedDate={selectedDate}
+                onDateChange={(selectedDate) => this.setState({ selectedDate })}
+              />
+            ) : (
+              <CancellationPath
+                selectedReservation={selectedReservation}
+                onReservationChange={(selectedReservation) =>
+                  this.setState({ selectedReservation })
+                }
+              />
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default MenuPage;
