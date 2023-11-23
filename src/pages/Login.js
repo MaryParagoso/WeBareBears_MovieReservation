be556527d197d91b3e41ed65814e082cc65daae2
deyynnaa <bacoldeannah.aws@gmail.com>
2023-11-23 17:15:01 +0800

    fist

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Button, Form } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-import { Input } from 'antd';
+import React from "react";
+import { Button, Form } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { Input } from "antd";
 // import 'antd/dist/antd.css'; // Import Ant Design styles
-import '../login.css';
+import "../stylesheets/index.css";
 
 const App: React.FC = () => (
-    <>
-      <Input size="large" placeholder="large size" prefix={<UserOutlined />} />
-      <br />
-      <br />
-      <Input placeholder="default size" prefix={<UserOutlined />} />
-      <br />
-      <br />
-      <Input size="small" placeholder="small size" prefix={<UserOutlined />} />
-    </>
-  );
-  
+  <>
+    <Input size="large" placeholder="large size" prefix={<UserOutlined />} />
+    <br />
+    <br />
+    <Input placeholder="default size" prefix={<UserOutlined />} />
+    <br />
+    <br />
+    <Input size="small" placeholder="small size" prefix={<UserOutlined />} />
+  </>
+);
+
 export default App;
