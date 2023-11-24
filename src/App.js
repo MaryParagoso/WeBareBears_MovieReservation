55673155d2348f9f51790bdc1e2136ae5f5d0d07
MaryParagoso <145335593+MaryParagoso@users.noreply.github.com>
2023-11-24 15:24:38 +0800

    Log In Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing';
+import Login from './pages/Login';
 import MenuPage from './pages/MenuPage';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Landing/>} ></Route>
+        <Route path="/login" element={<Login/>} ></Route>
         <Route path="/menu" element={<MenuPage/>} ></Route>
       </Routes>
     </Router>
