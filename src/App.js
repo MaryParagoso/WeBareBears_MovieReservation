d22ec7ae7075176cd68ac9f69563335132220039
deyynnaa <bacoldeannah.aws@gmail.com>
2023-11-27 08:26:29 +0800

    movie components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import MenuPage from './pages/MenuPage';
-import SeatLayout from './pages/SeatLayout';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import MenuPage from "./pages/MenuPage";
+import SeatLayout from "./pages/SeatLayout";
+import Movielist from "./pages/Movielist";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing/>} ></Route>
-        <Route path="/login" element={<Login/>} ></Route>
-        <Route path="/menu" element={<MenuPage/>} ></Route>
-        <Route path="/seat-layout" element={<SeatLayout/>} ></Route>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/menu" element={<MenuPage />}></Route>
+        <Route path="/seat-layout" element={<SeatLayout />}></Route>
+        <Route path="/movielist" element={<Movielist />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/pages/Movielist.js
@@ -0,0 +1,44 @@
+// Movielist.js
+import React, { useState } from "react";
+import {
+  Container,
+  Header,
+  Appname,
+  MovieImage,
+  Searchbar,
+  SearchIcon,
+  MovielistContainer,
+} from "../stylesheets/Cssmovielist";
+import SearchInput from "../component/SearchInput";
+import MovieComponent from "../component/MovieComp";
+import { moviesData } from "../component/MoviesData";
+
+function Movielist() {
+  const [searchQuery, updateSearchQuery] = useState("");
+
+  const onTextChange = (event) => {
+    updateSearchQuery(event.target.value);
+  };
+
+  return (
+    <Container>
+      <Header>
+        <Appname>
+          <MovieImage src="/clapperboard.png" alt="Clapperboard" />
+          Movie Lists
+        </Appname>
+        <Searchbar>
+          <SearchIcon src="/magnifying-glass.png" alt="MagnifyingGlass" />
+          <SearchInput onChange={onTextChange} />
+        </Searchbar>
+      </Header>
+      <MovielistContainer>
+        {moviesData.map((movie, index) => (
+          <MovieComponent key={index} movie={movie} />
+        ))}
+      </MovielistContainer>
+    </Container>
+  );
+}
+
+export default Movielist;
