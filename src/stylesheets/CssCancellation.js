b8b6f045f3ab3458dcf63ac38af2e93dc3a4c88e
deyynnaa <bacoldeannah.aws@gmail.com>
2023-11-28 16:36:05 +0800

    small changes on the cancellation path

--- src/stylesheets/CssCancellation.js
+++ src/stylesheets/CssCancellation.js
@@ -4,34 +4,34 @@ import styled from "styled-components";
 export const Container = styled.div`
   color: white;
   display: flex;
+  height: 100vh;
+  background-color: white; /* Background color of the page */
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  background-color: white;
 `;
 
 export const CancelContainer = styled.div`
   padding-top: 20px;
   border: 2px solid black;
   border-radius: 20px;
-  height: 600px;
+  height: 400px;
   width: 600px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0.586);
 `;
 
 export const CancelWallpaper = styled.div`
-  background-color: black;
   height: 100%;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background: url("theater.jpg") center/cover; /* Background image */
 `;
 
 export const SelectContainer = styled.div`
   width: 100%;
-  margin: 0 20px; // Adjust margin as needed
+  margin: 0 20px; /* Adjust margin as needed */
 `;
