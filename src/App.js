3777090f4b131b384ce8bf084723d4c2bdde7ee7
MaryParagoso <145335593+MaryParagoso@users.noreply.github.com>
2023-11-28 15:53:05 +0800

    Updated Login and Registration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
+import Registration from './pages/Registration';
 import MenuPage from "./pages/MenuPage";
 import SeatLayout from "./pages/SeatLayout";
 import Movielist from "./pages/Movielist";
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Landing />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/registration" element={<Registration />}></Route>
         <Route path="/menu" element={<MenuPage />}></Route>
         <Route path="/seat-layout" element={<SeatLayout />}></Route>
         <Route path="/movielist" element={<Movielist />}></Route>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,8 +3,7 @@ import React from "react";
 function Landing() {
   return (
     <div>
-      landing
-      <h1>Landing nivdaga</h1>
+      
     </div>
   );
 }
