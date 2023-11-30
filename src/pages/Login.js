d4209aff6a2c98a0bffb2f3d2ce0346003066a20
MaryParagoso <145335593+MaryParagoso@users.noreply.github.com>
2023-11-30 15:37:38 +0800

    Updates in UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Form, Input, Layout } from 'antd';
-import {headerStyle, contentStyle, footerStyle, formStyle, inputStyle, formItem } from '../stylesheets/mainUI';
+import { design, sectionTitle, contentStyle, headerStyle, footerStyle, formStyle, inputStyle, formItem, buttonLogin, buttonSignup } from '../stylesheets/mainUI';
 
 const { Header, Content, Footer } = Layout;
 
@@ -8,33 +8,35 @@ function Login() {
   return (
     <div>
       <Layout>
-        <Header style={headerStyle}>We Bare Bears</Header>
         <Content style={contentStyle}>
-          <h1 style={{ color: '#fff' }}>Log In to Your Account</h1>
-          <Form style={formStyle}>
-            <Form.Item 
-              style={formItem}
-              label="Username"
-              name="username"
-              rules={[{ required: true, message: 'Please input your username!' }]}>
-              <Input size="large" style={inputStyle} />
-            </Form.Item>
-            <Form.Item
-              style={formItem}
-              label="Password"
-              name="password"
-              rules={[{ required: true, message: 'Please input your password!' }]}>
-              <Input.Password size="large" style={inputStyle} />
-            </Form.Item>
-            <Form.Item style={{ display: 'flex', justifyContent: 'center' }}>
-              <Button ghost size="large" style={{ width: '150px', marginRight: '25px', color: '#000', borderColor: '#000' }}>
-                Sign Up
-              </Button>
-              <Button type="primary" size="large" htmlType="submit" style={{ width: '150px', backgroundColor: '#000' }}>
-                Log In
-              </Button>
-            </Form.Item>
-          </Form>
+          <div style={design}>
+            <Header style={headerStyle}>Movie Reservation System</Header>
+            <Form style={formStyle}>
+              <h1 style={sectionTitle}>Log In to Your Account</h1>
+              <Form.Item 
+                style={formItem}
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}>
+                <Input size="large" style={inputStyle} />
+              </Form.Item>
+              <Form.Item
+                style={formItem}
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}>
+                <Input.Password size="large" style={inputStyle} />
+              </Form.Item>
+              <Form.Item style={{ display: 'flex', justifyContent: 'center' }}>
+                <Button ghost size="large" style={buttonSignup}>
+                  Sign Up
+                </Button>
+                <Button type="primary" size="large" htmlType="submit" style={buttonLogin}>
+                  Log In
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
         </Content>
         <Footer style={footerStyle}>Ant Design ©2023 Created by We Bare Bears</Footer>
       </Layout>
