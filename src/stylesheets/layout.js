4388fdfddd4dc921ce28c05024cc55543e8b6f22
MaryParagoso <145335593+MaryParagoso@users.noreply.github.com>
2023-11-30 19:53:10 +0800

    Added UI in the ReservationConfirmation

--- src/stylesheets/layout.js
+++ src/stylesheets/layout.js
@@ -3,11 +3,15 @@ const overlayColor2 = 'rgba(0, 0, 0, 0.1)';
 const backgroundImageUrl = 'https://raw.githubusercontent.com/kishan0725/AJAX-Movie-Recommendation-System-with-Sentiment-Analysis/master/static/image.jpg';
 
 const contentStyle = {
-  padding: '0 50px',
-  minHeight: 'calc(100vh)',
+  padding: '0',
+  minHeight: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
   color: 'white',
   background: `linear-gradient(${overlayColor}, ${overlayColor}), url("${backgroundImageUrl}")`,
-  boxShadow: 'inset 0 0 100px rgba(0, 0, 0, 0.5)', 
+  boxShadow: 'inset 0 0 100px rgba(0, 0, 0, 0.5)',
 };
 
 const centerContent = { 
@@ -15,7 +19,13 @@ const centerContent = {
   flexDirection: 'column', 
   justifyContent: 'center', 
   alignItems: 'center', 
-  minHeight: '100vh' 
+  minHeight: '100vh', 
+  fontSize: '15px',
+}
+
+const Context = { 
+  fontSize: '20px',
+  margin: '8px', 
 }
 
 const Context1 = { 
@@ -90,7 +100,52 @@ const buttonSignup = {
   backdropFilter: 'blur(5px)',
 }
 
-export { overlayColor, overlayColor2, contentStyle, 
-  centerContent, Context1, Context2, breadcrumbStyle, 
-  breadcrumbContent, siteLayoutContent, footerStyle, 
-  buttonLogin, buttonSignup };
+const button1 = {
+  fontSize: '25px', 
+  width: '250px', 
+  height: '60px', 
+  marginRight: '50px', 
+  color: '#b20710', 
+  borderColor: '#b20710', 
+  background: 'rgba(255, 255, 255, 0.5)',
+  backdropFilter: 'blur(5px)',
+}
+
+const button2 = {
+  fontSize: '25px', 
+  width: '250px', 
+  height: '60px', 
+  backgroundColor: '#b20710', 
+  background: 'rgba(255, 0, 0, 0.4)',
+  backdropFilter: 'blur(5px)',
+}
+
+const reservationButton = {
+  fontSize: '23px',
+  width: '300px',
+  height: '45px',
+  color: 'white',
+  backgroundColor: '#b20710',
+  background: 'rgba(255, 0, 0, 0.5)',
+  backdropFilter: 'blur(8px)',
+  borderRadius: '10px',
+};
+
+const highlight = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: '5px',
+  boxShadow: '0 4px 8px rgba(255, 255, 255, 255, 0.1)',
+  padding: '10px 20px 10px',
+  borderRadius: '15px',
+  backgroundColor: 'rgba(90, 90, 90, 0.5)',
+  border: '1px solid red',
+}
+
+export { highlight, overlayColor, overlayColor2, contentStyle, 
+  centerContent, Context, Context1, Context2, 
+  breadcrumbStyle, breadcrumbContent, siteLayoutContent, 
+  footerStyle, buttonLogin, buttonSignup, 
+  button1, button2, reservationButton };
